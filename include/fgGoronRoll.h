85892c706321e9b727064b49597b72e4557e247b
PeteGrammarman <petegrammarman@gmail.com>
2026-02-14 00:12:29 -0700

    fixes to base dry bones, fat bones

---
+++ include/fgGoronRoll.h
@@ -0,0 +1,25 @@
+#ifndef FGGORONROLL_H
+#define FGGORONROLL_H
+
+#include "ultra64.h"
+#include "global.h"
+
+extern u64 Plane_karonnew2_exp_rgba16[];
+extern u64 Plane_pc55_eye_alb_0_transp_ci8[];
+extern u64 Plane_pc55_eye_alb_0_transp_pal_rgba16[];
+extern Vtx Plane_Plane_mesh_layer_Opaque_vtx_cull[8];
+extern Vtx Plane_Plane_mesh_layer_Opaque_vtx_0[118];
+extern Gfx Plane_Plane_mesh_layer_Opaque_tri_0[];
+extern Vtx Plane_Plane_mesh_layer_Opaque_vtx_1[4];
+extern Gfx Plane_Plane_mesh_layer_Opaque_tri_1[];
+extern Vtx Plane_Plane_mesh_layer_Opaque_vtx_2[4];
+extern Gfx Plane_Plane_mesh_layer_Opaque_tri_2[];
+extern Gfx mat_Plane_ka_body_f3d_layerOpaque[];
+extern Gfx mat_revert_Plane_ka_body_f3d_layerOpaque[];
+extern Gfx mat_Plane_f3dlite_material_042_layerOpaque[];
+extern Gfx mat_revert_Plane_f3dlite_material_042_layerOpaque[];
+extern Gfx mat_Plane_f3dlite_material_041_layerOpaque[];
+extern Gfx mat_revert_Plane_f3dlite_material_041_layerOpaque[];
+extern Gfx GoronPlane[];
+
+#endif
--- src/base.c
+++ src/base.c
@@ -2,16 +2,23 @@
 #include "global.h"
 #include "recomputils.h"
 #include "recompconfig.h"
+#include "segment_symbols.h"
 #include "playermodelmanager_api.h"
 #include "gDryBonesSkel.h"
 #include "gFatDryBonesSkel.h"
 #include "strongbad64.h"
+#include "fgGoronRoll.h"
+#include "gFishBonesSkel.h"
+#include "gDryNutSkel.h"
+#include "paulinia.h"
+
 
 // Mask positioning constants
 #define DB_MASK_SCALE_MODIFIER 1.2f
 #define DB_MASK_DOWNWARDS_OFFSET -14.f
-#define DB_MASK_FORWARDS_OFSSEET 650.f
+#define DB_MASK_FORWARDS_OFFSET 650.f
 #define DB_MASK_LATERAL_OFFSET 0.f
+
 
 //Empty Matrix, for removing shield
 Mtx gEmptyMtx;
@@ -32,59 +39,89 @@ extern Gfx* D_801C0B20[];  // Mask display lists (if needed)
 //Height stuff
 Player *gOriginalPlayer;
 u8 gOriginalPlayerTransformation;
+static Mtx DBMaskMtx; //Strong Bad mask matrix for positioning
+static Mtx DBSheathdMtx; //Dry Bones sheath matrix for positioning
+
+static Mtx sSBMaskMtx; //Strong Bad mask matrix for positioning
+static Mtx sSBShieldMtx; //Strong Bad shield matrix for positioning
+static Mtx FGDrumMtx; //Drum matrix for resizing
+
+extern Gfx GoronPlane[];
 
 
 PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerModels(void) {
+    //DRY BONES (human)
     PlayerModelManagerHandle h = PLAYERMODELMANAGER_REGISTER_MODEL("DryBonesTheSexGod", PMM_MODEL_TYPE_CHILD);
     PlayerModelManager_setAuthor(h, "PeteGrammarman"); // optional, will show up in a later version of PMM
     PlayerModelManager_setDisplayName(h, "Dry Bones"); // name that shows up in the menu
     PlayerModelManager_setSkeleton(h, &gDryBonesSkel);
     PlayerModelManager_setMatrix(h, PMM_MTX_SHIELD_HERO_BACK, &gEmptyMtx);
+    PlayerModelManager_setMatrix(h, PMM_MTX_SHIELD_MIRROR_BACK, &gEmptyMtx);
+    
+    //Mask poistioning
+    guPosition(&DBMaskMtx, 0.0f, 0.0f, 0.0f, DB_MASK_SCALE_MODIFIER, DB_MASK_FORWARDS_OFFSET, DB_MASK_DOWNWARDS_OFFSET, 0.0f);  //?,?,?,Scale, Forwards, Downwards, Lateral
+    PlayerModelManager_setMatrix(h, PMM_MTX_MASKS, &DBMaskMtx);
+    //Sheath positioning
+    //guPosition(&DBSheathMtx, 0.0f, 0.0f, 0.0f, 1.0f, 4.0f, - 12.0f, 0.0f); //?,?,?,Scale, Forwards, Downwards, Lateral
+    //PlayerModelManager_setMatrix(h, PMM_DL_SWORD_FIERCE_DEITY_BLADE, &DBSheathMtx;
 
+    //DRY BONES FAT (human)
     PlayerModelManagerHandle f = PLAYERMODELMANAGER_REGISTER_MODEL("DryBonesTheFatGod", PMM_MODEL_TYPE_CHILD);
     PlayerModelManager_setAuthor(f, "PeteGrammarman"); // optional, will show up in a later version of PMM
     PlayerModelManager_setDisplayName(f, "Fat Dry Bones"); // name that shows up in the menu
     PlayerModelManager_setSkeleton(f, &gFatDryBonesSkel);
     PlayerModelManager_setMatrix(f, PMM_MTX_SHIELD_HERO_BACK, &gEmptyMtx);
+    PlayerModelManager_setMatrix(f, PMM_MTX_SHIELD_MIRROR_BACK, &gEmptyMtx);
+    
 
+
+    //DRY BONES GORON
+    PlayerModelManagerHandle fg = PLAYERMODELMANAGER_REGISTER_MODEL("DryBonesTheFatGoron", PMM_MODEL_TYPE_GORON);
+    PlayerModelManager_setAuthor(fg, "PeteGrammarman"); // optional, will show up in a later version of PMM
+    PlayerModelManager_setDisplayName(fg, "Fat Goron Bones"); // name that shows up in the menu
+    PlayerModelManager_setSkeleton(fg, &gFatDryBonesSkel);
+    PlayerModelManager_setDisplayList(fg, PMM_DL_CURLED, GoronPlane); //Replace DL for Goron curled
+    PlayerModelManager_setDisplayList(fg, PMM_DL_CURLED, GoronPlane); 
+
+    //Resize Drums
+    /*
+    guPosition(&FGDrumMtx, 0.0f, 0.0f, 0.0f, 0.5f, 0.0f, 0.0f, 0.0f); //?,?,?,Scale, Forwards, Downwards, Lateral
+    PlayerModelManager_setMatrix(fg, PMM_DL_DRUM_STRAP, &FGDrumMtx);
+    PlayerModelManager_setMatrix(fg, PMM_DL_DRUM_LEFT, &FGDrumMtx);
+    PlayerModelManager_setMatrix(fg, PMM_DL_DRUM_RIGHT, &FGDrumMtx);
+    PlayerModelManager_setMatrix(fg, PMM_DL_DRUM_DOWN, &FGDrumMtx);
+    PlayerModelManager_setMatrix(fg, PMM_DL_DRUM_UP, &FGDrumMtx);
+    PlayerModelManager_setMatrix(fg, PMM_DL_DRUM_A, &FGDrumMtx);    
+    */
+
+
+    //FISH BONES ZORA
+    PlayerModelManagerHandle fz = PLAYERMODELMANAGER_REGISTER_MODEL("DryBonesTheFishBones", PMM_MODEL_TYPE_ZORA);
+    PlayerModelManager_setAuthor(fz, "PeteGrammarman"); // optional, will show up in a later version of PMM
+    PlayerModelManager_setDisplayName(fz, "Fish Bones"); // name that shows up in the menu
+    PlayerModelManager_setSkeleton(fz, &gFishBonesSkel);
+
+    //DRY NUT DEKU
+    PlayerModelManagerHandle dn = PLAYERMODELMANAGER_REGISTER_MODEL("DryBonesTheDryNut", PMM_MODEL_TYPE_DEKU);
+    PlayerModelManager_setAuthor(dn, "PeteGrammarman"); // optional, will show up in a later version of PMM
+    PlayerModelManager_setDisplayName(dn, "Dry Nut"); // name that shows up in the menu
+    PlayerModelManager_setSkeleton(dn, &gDryNutSkel);
+
+    
+    //STRONG BAD
     PlayerModelManagerHandle sb = PLAYERMODELMANAGER_REGISTER_MODEL("StrongBadTheWrestleMan", PMM_MODEL_TYPE_CHILD);
     PlayerModelManager_setAuthor(sb, "PeteGrammarman"); // optional, will show up in a later version of PMM
     PlayerModelManager_setDisplayName(sb, "Strong Bad"); // name that shows up in the menu
-    PlayerModelManager_setSkeleton(sb, &gStrongBadSkel_001);
+    PlayerModelManager_setSkeleton(sb, &StrongBadSkel);
+
+    //MASKS
+    guPosition(&sSBMaskMtx, 0.0f, 0.0f, 0.0f, 1.2f, 4.0f, - 12.0f, 0.0f);  //?,?,?,Scale, Forwards, Downwards, Lateral
+    PlayerModelManager_setMatrix(sb, PMM_MTX_MASKS, &sSBMaskMtx);
+
+    //SHIELD
+    guPosition(&sSBShieldMtx, 0.0f, 0.0f, 0.0f, 1.0f, -10.0f, 0.0f, 0.0f);  //?,?,?,Scale, Forwards, Downwards, Lateral
+    PlayerModelManager_setMatrix(sb, PMM_MTX_SHIELD1_ITEM, &sSBShieldMtx);
+    
 }
 
 
-RECOMP_HOOK("Player_PostLimbDrawGameplay") void on_Player_PostLimbDrawGameplay(PlayState* play, s32 limbIndex, Gfx** dList1, Gfx** dList2, Vec3s* rot, Actor* actor) {
-    Player* player = (Player*)actor;
-
-    // Handle mask positioning
-    if (limbIndex == PLAYER_LIMB_HEAD) {
-        if (((*dList1 != NULL) && ((u32)player->currentMask != PLAYER_MASK_NONE)) &&
-            (((player->transformation == PLAYER_FORM_HUMAN) &&
-              ((player->skelAnime.animation != &gPlayerAnim_cl_setmask) || (player->skelAnime.curFrame >= 12.0f))) ||
-             ((((player->transformation != PLAYER_FORM_HUMAN) && (player->currentMask >= PLAYER_MASK_FIERCE_DEITY)) &&
-               ((player->transformation + PLAYER_MASK_FIERCE_DEITY) != player->currentMask)) &&
-              (player->skelAnime.curFrame >= 10.0f)))) {   
-                s32 maskMinusOne = player->currentMask - 1;
-                OPEN_DISPS(play->state.gfxCtx);
-                Matrix_Push();
-                gPushedMatrixHead = 1;
-                Matrix_Scale(DB_MASK_SCALE_MODIFIER, DB_MASK_SCALE_MODIFIER, DB_MASK_SCALE_MODIFIER, MTXMODE_APPLY);
-                Matrix_Translate(DB_MASK_FORWARDS_OFSSEET, DB_MASK_DOWNWARDS_OFFSET, DB_MASK_LATERAL_OFFSET, MTXMODE_APPLY);
-                MATRIX_FINALIZE_AND_LOAD(POLY_OPA_DISP++, play->state.gfxCtx);
-                CLOSE_DISPS(play->state.gfxCtx);
-        } else {
-            gPushedMatrixHead = 0;
-        }    
-    } else {
-        gPushedMatrixSheath = 0;
-        gPushedMatrixHead = 0;
-    }
-}
-
-RECOMP_HOOK_RETURN("Player_PostLimbDrawGameplay") void return_Player_PostLimbDrawGameplay(void) {
-    if (gPushedMatrixHead) {
-        Matrix_Pop();
-        gPushedMatrixHead = 0;
-    }
-}
